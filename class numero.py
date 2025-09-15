66d56abcfca57334dd3752833ae1a1b287330bf4
171023angie <74705025@est.unap.edu.pe>
2025-09-15 08:33:25 -0500

    class numero

---
+++ class numero.py
@@ -0,0 +1,16 @@
+class Numero:
+    def __init__(self,valor):
+        self.valor = valor
+        
+    def clasificar(self):
+        if self.valor==0:
+           return "NULO"
+        elif self.valor%2==0:
+            return "Par"
+        else :
+            return "Impar"
+ejemplos = [Numero(0) , Numero(2) , Numero(5)]
+
+for num in ejemplos :
+    tipo = num.clasificar ()
+    print(f"Ele numero {num.valor} es {tipo} ")
