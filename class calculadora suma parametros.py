6f690ef65416f3aba7c8d8de1c6d3e8b449fcdbb
171023angie <74705025@est.unap.edu.pe>
2025-09-22 07:50:23 -0500

    Add files via upload

---
+++ class calculadora suma parametros.py
@@ -0,0 +1,12 @@
+class Calculadora:
+    def sumar (self,a,b):
+        sef.a=a
+        sef.b=b
+        # print(f"Valores: a = {a}, b = {b}")
+         return a+b
+
+
+calculadora = Calculadora()
+suma = calculadora.sumar(5, 7)
+
+print("La suma es:", suma)
